53ce5cb1f18a35636e6eaa10043716f96e65e7fc
Anton Lindgren <lindgren.anton@gmail.com>
2018-01-08 12:57:08 +0100

    Ignore falsy keys as they are not compatible with levelup

--- test/index.js
+++ test/index.js
@@ -3,7 +3,47 @@ const { expect } = require('chai');
 const KafkaCache = require('../lib/KafkaCache');
 const simple = require('simple-mock');
 
+const memdown = require('memdown');
+
 describe('KafkaCache unit-tests', function () {
+
+  afterEach(() => memdown.clearGlobalStore());
+
+  it('ignores payloads with missing keys', function (done) {
+
+    const logger = {
+      debug: simple.spy(),
+      error: simple.spy((...args) => console.error(...args)),
+      info: simple.spy(),
+      warn: simple.spy()
+    };
+
+    const mocks = {
+      stream: simple.mock()
+    };
+
+    const cache = new KafkaCache({
+      streamReady: Promise.resolve({ stream: mocks.stream, currentTopicOffset: -1 }),
+      log: logger,
+      levelupOptions: {
+        keyEncoding: 'utf-8',
+        valueEncoding: 'json'
+      },
+      resolver: x => x
+    });
+
+    const message = {
+      key: null,
+      value: Buffer.from(JSON.stringify({ foo: 'bar' }))
+    };
+
+    cache.onReady(() => {
+      mocks.stream.lastCall.arg(message);
+      expect(logger.error.lastCall.arg).to.contain('Falsy keys are not supported');
+
+      done();
+    });
+  });
 
   it('defaults to keyEncoding=string and valueEncoding=json', function (done) {
 
