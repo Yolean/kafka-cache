adf32fc6c58e87a4566ea389a244ac5a7a564f46
Anton Lindgren <lindgren.anton@gmail.com>
2018-08-07 15:12:46 +0200

    Add support for locally compressing and decompressing payloads

--- build-contracts/basics/index.js
+++ build-contracts/basics/index.js
@@ -66,20 +66,81 @@ describe('kafka-cache build-contract basics', function () {
     });
   });
 
-  it('doesnt transform buffer payloads in any way', async function () {
+  it('is possible to write compressed data with only the kafkaWrite api', async function () {
+    const kafkaWrite = KafkaCache.createKafkaWrite({
+      log,
+      topic: 'build-contract.basics.gzip',
+      kafkaHost: 'kafka:9092',
+      compressValues: true
+    });
+
+    const key1 = 'gzip_json_' + uuid.v4();
+    const key2 = 'gzip_json_' + uuid.v4();
+    const key3 = 'gzip_json_' + uuid.v4();
+
+    await kafkaWrite(key1, { test: 'blä1' });
+    await kafkaWrite(key2, { test: 'blä2' });
+    await kafkaWrite(key3, { test: 'blä3' });
 
     const cache = KafkaCache.create({
       kafkaHost: 'kafka:9092', // We never talked about this one but I guess it's required nonetheless
-      topic: 'build-contract.basics.buffer',
+      topic: 'build-contract.basics.gzip',
       readOnly: false,
-      valueEncoding: 'json'
+      valueEncoding: 'binary',
+      compressValues: true,
+      log
     });
 
     await cache.onReady();
-    await cache.put('foo.bar', Buffer.from(JSON.stringify({ foo: 'bar' })));
-    await new Promise(resolve => cache.on('put', () => resolve()));
-    const foo = await cache.get('foo.bar');
-    expect(foo).to.equal(Buffer.from(JSON.stringify({ foo: 'bar' })));
+
+    const val1 = await cache.get(key1);
+    expect(val1).to.deep.equal(Buffer.from(JSON.stringify({ test: 'blä1' })));
+    const val2 = await cache.get(key2);
+    expect(val2).to.deep.equal(Buffer.from(JSON.stringify({ test: 'blä2' })));
+    const val3 = await cache.get(key3);
+    expect(val3).to.deep.equal(Buffer.from(JSON.stringify({ test: 'blä3' })));
+  });
+
+  it('is also possible to put and get compressed values within the same kafka-cache instance', async function () {
+
+    const cache = KafkaCache.create({
+      kafkaHost: 'kafka:9092', // We never talked about this one but I guess it's required nonetheless
+      topic: 'build-contract.basics.gzip',
+      readOnly: false,
+      valueEncoding: 'json',
+      compressValues: true,
+      log
+    });
+
+    await cache.onReady();
+
+    const key1 = 'gzip_json_1_' + uuid.v4();
+    const key2 = 'gzip_json_2_' + uuid.v4();
+    const key3 = 'gzip_json_3_' + uuid.v4();
+
+    const receivePut = (key) => new Promise(resolve => cache.on('put', (id) => {
+      if (id.toString() === key) resolve();
+    }));
+
+    // Start watching for puts received before putting
+    const putsReceived = [
+      receivePut(key1),
+      receivePut(key2),
+      receivePut(key3),
+    ];
+
+    cache.put(key1, { test: 'blä1' });
+    cache.put(key2, { test: 'blä2' });
+    cache.put(key3, { test: 'blä3' });
+
+    await Promise.all(putsReceived);
+
+    const val1 = await cache.get(key1);
+    expect(val1).to.deep.equal({ test: 'blä1' });
+    const val2 = await cache.get(key2);
+    expect(val2).to.deep.equal({ test: 'blä2' });
+    const val3 = await cache.get(key3);
+    expect(val3).to.deep.equal({ test: 'blä3' });
   });
 
   it('supports gzip compression');
