5f12162b5e456d5345e972b61acd3f3593703e99
Anton Lindgren <lindgren.anton@gmail.com>
2019-01-02 18:03:27 +0100

    Add unit-tests for waitForOffset

--- test/KafkaCache.spec.js
+++ test/KafkaCache.spec.js
@@ -14,6 +14,68 @@ function createMockMetricsApi() {
 
 describe('KafkaCache unit-tests', function () {
 
+  it('exposes an additional .waitForOffset functionality that becomes important when kafka is backing a cache', async function () {
+    const logger = {
+      debug: simple.spy(),
+      error: simple.spy((...args) => console.error(...args)),
+      info: simple.spy(),
+      warn: simple.spy()
+    };
+
+    const mocks = {
+      stream: simple.mock()
+    };
+
+    const metricsApi = createMockMetricsApi();
+
+    const cache = new KafkaCache({
+      streamReady: Promise.resolve({ stream: mocks.stream, currentTopicOffset: -1 }),
+      log: logger,
+      levelupOptions: {
+        keyEncoding: 'utf-8',
+        valueEncoding: 'json'
+      },
+      resolver: x => x,
+      metrics: metricsApi
+    });
+
+    await cache.onReady();
+
+    const produceFlakyMessage = (message) => {
+      setTimeout(() => {
+        mocks.stream.lastCall.arg(message);
+      }, Math.random() * 50);
+    }
+
+    // Support listeners registered before the offset is present
+    const offsetReady = cache.waitForOffset(0);
+    produceFlakyMessage({
+      key: 'key1',
+      value: Buffer.from(JSON.stringify({ foo: 'bar' })),
+      offset: 0
+    });
+    await offsetReady;
+    const value = await cache.get('key1');
+    expect(value).to.deep.equal({ foo: 'bar' });
+
+    // Support listeners registered after the offset was already received
+    produceFlakyMessage({
+      key: 'key2',
+      value: Buffer.from(JSON.stringify({ foo: 'bar' })),
+      offset: 2
+    });
+    produceFlakyMessage({
+      key: 'key2',
+      value: Buffer.from(JSON.stringify({ foo: 'bar' })),
+      offset: 3
+    });
+    await offsetReady;
+    await cache.waitForOffset(3);
+    await cache.waitForOffset(2);
+    const value2 = await cache.get('key2');
+    expect(value2).to.deep.equal({ foo: 'bar' });
+  });
+
   it('ignores payloads with missing keys but logs them and reports them as metrics', async function () {
 
     const logger = {
